1f59e23009d45c168210fd8d4ca71344e4b8baa1
Subin Park <134358849+suzzang2@users.noreply.github.com>
2025-04-05 18:23:05 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+
+using namespace std;
+
+int dp[21][21][21];
+
+// w 함수
+int w(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0)
+        return 1;
+    if (a > 20 || b > 20 || c > 20)
+        return w(20, 20, 20);
+
+    if (dp[a][b][c] != -1)
+        return dp[a][b][c];
+
+    if (a < b && b < c)
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+    else
+        dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+
+    return dp[a][b][c];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // -1로 초기화
+    fill(&dp[0][0][0], &dp[20][20][21], -1);
+
+    while (true) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == -1 && b == -1 && c == -1)
+            break;
+
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << "\n";
+    }
+
+    return 0;
+}
