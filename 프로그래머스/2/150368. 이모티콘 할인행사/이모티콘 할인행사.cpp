d890ca9d22fa8eb0a4f84cdf4e7e81a160d4783b
Subin Park <134358849+suzzang2@users.noreply.github.com>
2025-05-03 02:41:12 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 30.52 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
+    vector<int> answer;
+    return answer;
+}
