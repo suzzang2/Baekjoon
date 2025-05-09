c0772cfa88fec2bf55b90d92a5624858b8d1b400
Subin Park <134358849+suzzang2@users.noreply.github.com>
2025-05-09 13:33:11 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,20 +1,24 @@
-#include <vector>
-#include <algorithm>
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
-int main(void){
-   int n; string str;
-   cin >> n; cin >> str;
+const int MOD = 1234567891;
 
-   int sum=0;
-   //알파벳을 숫자로 매핑
-   for(int i=0; i<n; ++i){
-      int num = str[i]-'a'+1;
-      sum += num*pow(31,i);
-   }
-   cout << sum;
-   return 0;
+int main() {
+    int n;
+    string str;
+    cin >> n >> str;
+
+    long long sum = 0;
+    long long power = 1;
+
+    for(int i = 0; i < n; ++i) {
+        int num = str[i] - 'a' + 1;
+        sum = (sum + num * power) % MOD;
+        power = (power * 31) % MOD;
+    }
+
+    cout << sum;
+    return 0;
 }
